5822ab223d493e7e9e81d5f58af91ac4778590b7
sudharaD <sudharad7@gmail.com>
2022-09-18 00:59:31 +0530

    Scaffold created

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello World'),
+        ),
+        body: Container(
+          color: Colors.red,
+        ),
+      ),
+    ),
+  );
+}
